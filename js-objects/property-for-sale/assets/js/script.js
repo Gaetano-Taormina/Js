ba054d94c177702f0c4531601707c14f4186a8c1
Gaetano Taormina <261669727+Gaetano-Taormina@users.noreply.github.com>
2026-06-20 12:11:31 +0200

    Implement essential logic for property-for-sale

---
+++ js-objects/property-for-sale/assets/js/script.js
@@ -0,0 +1,11 @@
+console.log("Script loaded correctly");
+
+const property = {
+    city: "Rome",
+    squareMeters: 85,
+    rooms: 3,
+    price: 250000,
+    hasBalcony: true
+};
+
+console.log(property);
