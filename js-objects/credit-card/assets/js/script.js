f7d1ca6845b7bf448a1b1943d9c8594d3c905ca2
Gaetano Taormina <gaetanotao04@gmail.com>
2026-06-20 12:11:25 +0200

    Implement essential logic for credit-card

---
+++ js-objects/credit-card/assets/js/script.js
@@ -0,0 +1,11 @@
+console.log("Script loaded correctly");
+
+const creditCard = {
+    owner: "Mario Rossi",
+    number: "1234-5678-9012-3456",
+    expirationMonth: 12,
+    expirationYear: 2028,
+    cvv: 123
+};
+
+console.log(creditCard);
