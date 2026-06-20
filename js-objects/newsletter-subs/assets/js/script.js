88d88689506b39f0f2664623e40c013be6192244
Gaetano Taormina <261669727+Gaetano-Taormina@users.noreply.github.com>
2026-06-20 12:11:29 +0200

    Implement essential logic for newsletter-subs

---
+++ js-objects/newsletter-subs/assets/js/script.js
@@ -0,0 +1,8 @@
+const newsletter = {
+  name: "Tech News",
+  subscribers: [
+    { email: "a@test.com", active: true },
+    { email: "b@test.com", active: false }
+  ]
+};
+console.log(newsletter);
