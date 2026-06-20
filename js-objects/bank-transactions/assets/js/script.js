9d734326c6089ee1fa5885a546232afbe02c9ca3
Gaetano Taormina <gaetanotao04@gmail.com>
2026-06-20 12:11:23 +0200

    Implement essential logic for bank-transactions

---
+++ js-objects/bank-transactions/assets/js/script.js
@@ -0,0 +1,9 @@
+const account = {
+  owner: "Dave",
+  balance: 1000,
+  transactions: [
+    { type: "deposit", amount: 500 },
+    { type: "withdrawal", amount: 200 }
+  ]
+};
+console.log(account);
