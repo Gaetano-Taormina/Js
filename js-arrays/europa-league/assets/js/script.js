df3d0eebafea6ca1d3b4f48199a53a0c4897cb39
Gaetano Taormina <261669727+Gaetano-Taormina@users.noreply.github.com>
2026-06-20 12:10:30 +0200

    Implement essential logic for europa-league

---
+++ js-arrays/europa-league/assets/js/script.js
@@ -0,0 +1,3 @@
+const standings = ["Team1", "Team2", "Team3", "Team4", "Team5", "Team6", "Team7", "Team8", "Team9", "Team10"];
+const europaLeague = standings.slice(4, 8);
+console.log("Europa League:", europaLeague);
