8cd91ecd8630e43befc239875e5a1a49805ac86c
Gaetano Taormina <gaetanotao04@gmail.com>
2026-06-20 12:11:29 +0200

    Implement essential logic for physical-person

---
+++ js-objects/physical-person/assets/js/script.js
@@ -0,0 +1,11 @@
+console.log("Script loaded correctly");
+
+const person = {
+    name: "Fabio" ,
+    surname: "Tozzi",
+    age: 22 ,
+    height: 173 ,
+    weight: 78
+}
+
+console.log(person) ;
