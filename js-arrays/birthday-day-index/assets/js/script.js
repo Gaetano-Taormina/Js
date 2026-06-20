11b02bf77835df7de62b1476d694d267b810ceaa
Gaetano Taormina <261669727+Gaetano-Taormina@users.noreply.github.com>
2026-06-20 12:10:26 +0200

    Implement essential logic for birthday-day-index

---
+++ js-arrays/birthday-day-index/assets/js/script.js
@@ -0,0 +1,5 @@
+console.log("Script loaded correctly");
+
+let giorni = ["Lunedì", "Martedì", "Mercoledì", "Giovedì", "Venerdì", "Sabato", "Domenica"];
+let giornoCompleanno = "Giovedì";
+console.log("Indice del giorno: " + giorni.indexOf(giornoCompleanno));
