1fdd317ba4142cdb39cf702c353918a22c3e7fe0
Gaetano Taormina <261669727+Gaetano-Taormina@users.noreply.github.com>
2026-06-20 12:11:34 +0200

    Implement essential logic for training-course

---
+++ js-objects/training-course/assets/js/script.js
@@ -0,0 +1,10 @@
+console.log("Script loaded correctly");
+
+const course = {
+    title: "JavaScript Basics",
+    instructor: "Luca Bianchi",
+    durationHours: 40,
+    maxStudents: 25
+};
+
+console.log(course);
