ef8c58cc2f01c7c0713b960e259ba87f589d4520
Gaetano Taormina <261669727+Gaetano-Taormina@users.noreply.github.com>
2026-06-20 12:11:01 +0200

    Implement essential logic for club-access

---
+++ js-conditions/club-access/assets/js/script.js
@@ -0,0 +1,10 @@
+console.log("Script loaded correctly");
+
+let age = prompt("Enter your age: ");
+age = Number(age);
+
+if (age >= 18){
+    console.log("Access granted to the club");
+} else {
+    console.log("Access denied");
+}
