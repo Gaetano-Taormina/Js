b07643c036de38b0e70476d8589733137ee58b9f
Gaetano Taormina <gaetanotao04@gmail.com>
2026-06-20 12:10:34 +0200

    Implement essential logic for parking-locator

---
+++ js-arrays/parking-locator/assets/js/script.js
@@ -0,0 +1,5 @@
+console.log("Script loaded correctly");
+
+let targhe = ["AA123BB", "CC456DD", "EE789FF"];
+let targaCercata = "CC456DD";
+console.log("L'auto si trova al posto numero: " + targhe.indexOf(targaCercata));
