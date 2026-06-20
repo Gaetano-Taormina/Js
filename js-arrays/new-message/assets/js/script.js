9a61ecca2850dfafe20b7883f235742feba64386
Gaetano Taormina <gaetanotao04@gmail.com>
2026-06-20 12:10:32 +0200

    Implement essential logic for new-message

---
+++ js-arrays/new-message/assets/js/script.js
@@ -0,0 +1,6 @@
+console.log("Script loaded correctly");
+
+let conversazione = ["Ciao", "Come stai?"];
+let nuovoMessaggio = "Tutto bene!";
+conversazione.push(nuovoMessaggio);
+console.log(conversazione);
