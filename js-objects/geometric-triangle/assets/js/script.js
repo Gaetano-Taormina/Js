03ab210d4306c259fb2c50d95b3763f0fb0acca9
Gaetano Taormina <261669727+Gaetano-Taormina@users.noreply.github.com>
2026-06-20 12:11:27 +0200

    Implement essential logic for geometric-triangle

---
+++ js-objects/geometric-triangle/assets/js/script.js
@@ -0,0 +1,11 @@
+console.log("Script loaded correctly");
+
+const triangle = {
+    sides: 3,
+    vertices: 3,
+    internalAnglesSum: 180,
+    shape: "polygon",
+    is2D: true
+};
+
+console.log(triangle);
