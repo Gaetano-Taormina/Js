c6537fc8d40b0653298afe006747c67020c2fac3
Gaetano Taormina <261669727+Gaetano-Taormina@users.noreply.github.com>
2026-06-20 12:11:17 +0200

    Implement essential logic for guess-number

---
+++ js-loops/guess-number/assets/js/script.js
@@ -0,0 +1,8 @@
+console.log("Script loaded correctly");
+
+let segreto = 7;
+let tentativo = -1;
+while (tentativo != segreto) {
+    tentativo = prompt("Indovina il numero segreto:");
+}
+console.log("Hai indovinato!");
