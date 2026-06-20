8da80662b465de871e4e5a860e633fa7fdaf670d
Gaetano Taormina <261669727+Gaetano-Taormina@users.noreply.github.com>
2026-06-20 12:11:24 +0200

    Implement essential logic for calendar-event

---
+++ js-objects/calendar-event/assets/js/script.js
@@ -0,0 +1,10 @@
+console.log("Script loaded correctly");
+
+const calendarEvent = {
+    title: "Dentist Appointment",
+    date: "2026-06-20",
+    time: "15:30",
+    location: "Via Roma 10"
+};
+
+console.log(calendarEvent);
