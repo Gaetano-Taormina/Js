2012410eef3b95c6981ec7b6ae33deba3acd7174
Gaetano Taormina <gaetanotao04@gmail.com>
2026-06-20 12:11:19 +0200

    Implement essential logic for repeat-word

---
+++ js-loops/repeat-word/assets/js/script.js
@@ -0,0 +1,7 @@
+console.log("Script loaded correctly");
+
+let parola = prompt("Inserisci una parola:");
+let n = prompt("Quante volte vuoi stamparla?");
+for (let i = 0; i < n; i++) {
+    console.log(parola);
+}
