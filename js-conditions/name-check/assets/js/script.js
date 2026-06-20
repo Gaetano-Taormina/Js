3fb310ded810aec11f2a60df2e85884a182615d8
Gaetano Taormina <gaetanotao04@gmail.com>
2026-06-20 12:11:06 +0200

    Implement essential logic for name-check

---
+++ js-conditions/name-check/assets/js/script.js
@@ -0,0 +1,9 @@
+console.log("Script loaded correctly");
+
+let name = prompt("Enter user's name: ");
+
+if (!name) {
+    console.log("You have not entered any name");
+} else {
+    console.log("The user's name is: ", name);
+}
