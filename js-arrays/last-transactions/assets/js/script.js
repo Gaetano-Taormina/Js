dda6049ed3290ef01a5f25a7ea314426a43159a9
Gaetano Taormina <261669727+Gaetano-Taormina@users.noreply.github.com>
2026-06-20 12:10:32 +0200

    Implement essential logic for last-transactions

---
+++ js-arrays/last-transactions/assets/js/script.js
@@ -0,0 +1,5 @@
+console.log("Script loaded correctly");
+
+let transazioni = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+let ultime10 = transazioni.slice(-10);
+console.log("Ultime 10:", ultime10);
