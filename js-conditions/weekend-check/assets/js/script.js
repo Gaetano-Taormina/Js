da5ce5e42f7488eae077e47a91626971f1c85bbb
Gaetano Taormina <261669727+Gaetano-Taormina@users.noreply.github.com>
2026-06-20 12:11:10 +0200

    Implement essential logic for weekend-check

---
+++ js-conditions/weekend-check/assets/js/script.js
@@ -0,0 +1,9 @@
+console.log("Script loaded correctly");
+
+let day = prompt("Enter the day of the week: ");
+
+if (day === "saturday" || day === "sunday" || day === "Saturday" || day === "Sunday") {
+    console.log("Have a good weekend");
+} else {
+    console.log("Have a good day at work");
+}
