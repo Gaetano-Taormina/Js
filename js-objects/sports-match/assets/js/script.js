7b49714605d5dfde500d4dbf6a8e314729d36d6a
Gaetano Taormina <261669727+Gaetano-Taormina@users.noreply.github.com>
2026-06-20 12:11:33 +0200

    Implement essential logic for sports-match

---
+++ js-objects/sports-match/assets/js/script.js
@@ -0,0 +1,11 @@
+console.log("Script loaded correctly");
+
+const match = {
+    homeTeam: "Milan",
+    awayTeam: "Inter",
+    homeScore: 2,
+    awayScore: 1,
+    isLive: true
+};
+
+console.log(match);
